94723c8208daf4653ae49248751214eff26d3ef0
highrankprogrammer <highrankdev@gmail.com>
2014-07-10 14:04:44 -0700

    add gform widget

---
+++ gformNamesToPlaceHolders.js
@@ -0,0 +1,22 @@
+var gformNamesToPlaceHolders = function() {
+	function getGformName(s) {
+		if (s.indexOf("gfield_error") == -1) {
+			var n = s.lastIndexOf(" ");
+			s = s.slice(10,n);
+			n = s.indexOf(" ");
+			s = s.slice(0,n);
+			return s;
+		} else {
+			var n = s.lastIndexOf(" ");
+			s = s.slice(21,n);
+			return s;		
+		}
+	}
+	var b = document.getElementById("gform_fields_1").childNodes;
+	var inputs = document.getElementsByClassName("ginput_container");
+	for (var i = 0; i < inputs.length; i++) {
+		inputs[i].firstChild.setAttribute
+			("placeholder",
+			getGformName(inputs[i].parentNode.getAttribute("class")));
+	}
+}
